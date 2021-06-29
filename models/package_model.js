bb13e5c90a2ba2d268f76695f47e3ed1a0a09ab9
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-06-29 16:52:06 -0400

    creates package model

--- models/package_model.js
+++ models/package_model.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TagSchema = new Schema({
+	name: String,
+});
+
+const packageSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		image: { type: String, required: true },
+		country: { type: String, required: true },
+		city: { type: String, required: true },
+		address: { type: String, required: true },
+		guestNumber: { type: Number, required: true },
+		price: { type: Number, required: true },
+		tags: [TagSchema],
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+	},
+	{ timestamps: true }
+);
+
+const Package = mongoose.model('Package', packageSchema);
+
+module.exports = Package;
