26cd41697cfe786b428b43ec4bd56aa3eaf2354e
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-06-29 18:03:48 -0400

    adds user data seeder

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,12 +2,24 @@ const express = require('express');
 const userRouter = express.Router();
 const User = require('../models/user_model');
 const Package = require('../models/package_model');
+const seedData = require('../seeders/user_seed_data');
 
 //index
 userRouter.get('/', async (req, res) => {
 	try {
 		const users = await User.find({});
 		res.json({ users });
+	} catch (error) {
+		res.status(400).json(error);
+	}
+});
+
+//seeder
+userRouter.post('/seeder', async (req, res) => {
+	try {
+		//add deletion here
+		req.body = seedData;
+		res.json(await User.create(req.body));
 	} catch (error) {
 		res.status(400).json(error);
 	}
---
+++ seeders/user_seed_data.js
@@ -0,0 +1,20 @@
+module.exports = [
+	{
+		name: 'admin',
+		email: 'admin@yahoo.com',
+		password: '123456',
+		isAdmin: true,
+	},
+	{
+		name: 'john doe',
+		email: 'john@yahoo.com',
+		password: '123456',
+		isAdmin: false,
+	},
+	{
+		name: 'jane doe',
+		email: 'jane@yahoo.com',
+		password: '123456',
+		isAdmin: false,
+	},
+];
