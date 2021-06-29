c03eee33add3afc140a34a20dda2179dbe546239
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-06-29 17:17:52 -0400

    creates booking model

---
+++ models/booking_model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema(
+	{
+		bookingItems: [
+			{
+				price: { type: Number, required: true },
+				name: { type: String, required: true },
+				image: { type: String, required: true },
+				isComplete: { type: Boolean, required: true, default: false },
+				date: { type: Date, required: true },
+				time: { type: Date, required: true },
+				package: {
+					type: Schema.Types.ObjectId,
+					required: true,
+					ref: 'Package',
+				},
+			},
+		],
+		user: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		totalPrice: { type: Number, required: true },
+		isPaid: { type: Boolean, required: true, default: false },
+	},
+	{ timestamps: true }
+);
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
