4fd6c68b6be80b60644497ac174faf1601e1dce2
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-06-30 16:34:04 -0400

    updates package data seeder

--- seeders/package_seed_data.js
+++ seeders/package_seed_data.js
@@ -3,7 +3,8 @@ module.exports = [
 		title: 'Airport Pickup',
 		description:
 			'Save yourself the hassle of navigationg public transportation or booking a taxi. Our experienced drivers will be waiting for you at the arrivals lounge and take you right to your hotel!',
-		image: { type: String, required: true },
+		image:
+			'https://images.unsplash.com/photo-1521136095380-08fbd7be93c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
 		country: 'Brazil',
 		city: 'Rio de Janeiro',
 		address: 'Tom Jobim International Airport',
@@ -15,7 +16,8 @@ module.exports = [
 		title: 'Sugar Loaf Mountain Tour',
 		description:
 			'Jutting up from the ocean, Sugar Loaf is a must see destination in Rio! Take in spectacular views of the city from Urca and Sugar loaf peaks while walking through lush scenery. Accessible by cable car or hiking (up to Mt. Urca).',
-		image: { type: String, required: true },
+		image:
+			'https://images.unsplash.com/photo-1561577553-674ce32847a4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNvcmNvdmFkb3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
 		country: 'Brazil',
 		city: 'Rio de Janeiro',
 		address: 'Av. Pasteur, 520 - Urca, Rio de Janeiro',
@@ -24,15 +26,16 @@ module.exports = [
 		createdBy: '',
 	},
 	{
-		title: 'Airport Pickup',
+		title: 'Christ the Redeemer/Corcovado',
 		description:
-			'Save yourself the hassle of navigationg public transportation or booking a taxi. Our experienced drivers will be waiting for you at the arrivals lounge and take you right to your hotel!',
-		image: { type: String, required: true },
+			"Rio's iconic landmark is a must see for anyone visiting this vibrant city! Take the scenic train ride up to the summit of Corcovado and visit the Christ the Redeemer statue. ",
+		image:
+			'https://images.unsplash.com/photo-1595688878177-b72dfeeed683?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvcmNvdmFkb3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
 		country: 'Brazil',
 		city: 'Rio de Janeiro',
-		address: 'Tom Jobim International Airport',
-		price: 20,
-		tags: ['Transportation', 'Airport', 'Rio'],
+		address: 'R. Cosme Velho, 513 - Cosme Velho, Rio de Janeiro',
+		price: 25,
+		tags: ['Views', 'Hikes', 'Nature', 'Rio'],
 		createdBy: '',
 	},
 ];
