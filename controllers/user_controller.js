d15f92b5863d90454a4f8090c791841e3600bfcf
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-01 13:16:47 -0400

    adds jwt and login route

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/user_model');
 const Package = require('../models/package_model');
-const seedData = require('../seeders/user_seed_data');
+let seedData = require('../seeders/user_seed_data');
 
 //index
 userRouter.get('/', async (req, res) => {
@@ -14,12 +17,55 @@ userRouter.get('/', async (req, res) => {
 	}
 });
 
+//login
+userRouter.post('/login', async (req, res) => {
+	try {
+		if (req.body.password) {
+			const user = await User.findOne({
+				email: req.body.email,
+			});
+			if (!user) {
+				res.json({
+					message: 'No user with that email address has been registered',
+				});
+			} else {
+				const passwordMatches = bcrypt.compareSync(
+					req.body.password,
+					user.password
+				);
+
+				if (passwordMatches) {
+					jwt.sign(
+						{ user },
+						process.env.SECRET,
+						{ expiresIn: '1h' },
+						(err, token) => {
+							res.json({ token, message: 'successful login' });
+						}
+					);
+				} else {
+					console.log('passwords did not match');
+					res.json({ message: 'incorrect password' });
+				}
+			}
+		} else {
+			res.json('enter password');
+		}
+	} catch (err) {
+		res.status(400).json(error);
+	}
+});
+
 //seeder
 userRouter.post('/seeder', async (req, res) => {
 	try {
 		await User.deleteMany({});
+		seedData.forEach((user) => {
+			user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+		});
 		req.body = seedData;
-		res.json(await User.create(req.body));
+		const users = await User.create(req.body);
+		res.json({ users });
 	} catch (error) {
 		res.status(400).json(error);
 	}
