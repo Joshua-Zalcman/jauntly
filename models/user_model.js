d7c2bc5d6b274840e7fd8df322edf57e82fced4b
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-01 17:09:04 -0400

    updates user model

--- models/user_model.js
+++ models/user_model.js
@@ -11,6 +11,15 @@ const userSchema = new Schema(
 	{ timestamps: true }
 );
 
+//middleware
+userSchema.set('toJSON', {
+	transform: function (doc, ret) {
+		// remove the password property when serializing doc to JSON
+		delete ret.password;
+		return ret;
+	},
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
